f00d85cc3c6df38a21d345e50303c45da2eb02bf
artuknees <artuknees@gmail.com>
2023-05-06 13:06:24 -0300

    modularized images component

--- components/Home/FeatureImages.jsx
+++ components/Home/FeatureImages.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import ilustration1 from '../../public/ilustration1.png';
-import ilustration2 from '../../public/ilustration2.png';
-import ilustration3 from '../../public/ilustration3.png';
 import { Typography, createTheme , ThemeProvider } from "@mui/material";
-
 
 const FeatureImages = () => {
     const typographyStyle = createTheme();
@@ -24,50 +20,31 @@ const FeatureImages = () => {
         fontSize: '1.6rem',
       }
     };
+    const titles = ["Stay Up-to-Date","Find Your Perfect Fit","Shop by Color and Material"];
+    const subTitles = [
+        "Stay ahead of the curve with Dokuso's trend analysis.",
+        "Get recommendations based on your body type and style preferences.",
+        "Find exactly what you're looking for with Dokuso's advanced filtering options."
+    ];
 
     return (
         <div className="flex flex-col items-center w-full pt-16">
-
             <div className='flex lg:flex-row flex-wrap w-full items-center justify-center'>
-                <div className='w-[300px] flex flex-col flex-none items-center m-5'>
-                    <Image src={ilustration1} width={150} height={150} alt='Illustration 1'/>
-                    <ThemeProvider theme={typographyStyle}>
-                        <Typography gutterBottom 
-                            variant="h2" 
-                            component="h2" 
-                        >
-                            Stay Up-to-Date
-                        </Typography>
-                    </ThemeProvider>
-                    <p className="text-center">Stay ahead of the curve with Dokuso's trend analysis.</p>
-                </div>
-                <div className='w-[300px] flex flex-col flex-none items-center m-5'>
-                    <Image src={ilustration2} width={150} height={150} alt='Illustration 2' />
-                    <ThemeProvider theme={typographyStyle}>
-                        <Typography gutterBottom 
-                            variant="h2" 
-                            component="h2" 
-                        >
-                            Find Your Perfect Fit
-                        </Typography>
-                    </ThemeProvider>
-                    <p className="text-center">Get recommendations based on your body type and style preferences.</p>
-                </div>
-                <div className='w-[300px] flex flex-col flex-none items-center m-5'>
-                    <Image src={ilustration3} width={150} height={150} alt='Illustration 3' />
-                    <ThemeProvider theme={typographyStyle}>
-                        <Typography gutterBottom 
-                            variant="h2" 
-                            component="h2" 
-                        >
-                            Shop by Color and Material
-                        </Typography>
-                    </ThemeProvider>
-                    <p className="text-center">Find exactly what you're looking for with Dokuso's advanced filtering options.</p>
-                </div>
+                {titles.map((itemTitle, indexTitle) => {return (
+                    <div key={indexTitle} className='w-[300px] flex flex-col flex-none items-center m-5'>
+                        <Image src={`/ilustration${indexTitle+1}.png`} className="mb-5" width={150} height={150} alt={`illustration${indexTitle+1}`}/>
+                        <ThemeProvider theme={typographyStyle}>
+                            <Typography gutterBottom 
+                                variant="h2" 
+                                component="h2" 
+                            >
+                                {itemTitle}
+                            </Typography>
+                        </ThemeProvider>
+                        <p className="text-center">{subTitles[indexTitle]}</p>
+                    </div>
+                )})}
             </div>
-
-
         </div>
     )
 };
