b536e57a617d9554e9e02aa26dcae0a2994434df
Arturo <artuknees@gmail.com>
2023-05-09 08:46:33 -0300

    working google auth

--- components/Auth/signInWithGoogleExternal.js
+++ components/Auth/signInWithGoogleExternal.js
@@ -6,23 +6,27 @@ const provider = new GoogleAuthProvider();
 export const signInGoogleExternal = async (auth,db) => { // basic sign in function
   try {
       const result = await signInWithPopup(auth,provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
       const user = result.user;
       const q = query(collection(db, "users"), where("uid", "==", user.uid)); // bring all users with their UID
       const docs = await getDocs(q);
       if (docs.docs.length === 0) { // If theres no user with the current UID
-        await addDoc(collection(db, "users"), { // create a user with the current UID
-          uid: user.uid,
-          name: user.displayName,
-          authProvider: "google",
-          email: user.email,
-        });
-        console.log('Signed up with Google - Added new user')
-        // event login - add later
-        // logEvent(analytics, 'sign_up', {
-        //   method: 'google'
-        // });
+        try {
+          await addDoc(collection(db, "users"), { // create a user with the current UID
+            uid: user.uid,
+            name: user.displayName,
+            authProvider: "google",
+            email: user.email,
+          });
+          // event login - add later
+          // logEvent(analytics, 'sign_up', {
+          //   method: 'google'
+          // }); 
+          console.log('Logged in with Google - new user created'); 
+        } catch (err) {
+          console.error(err);
+        }
       } else {
         console.log('Logged in with Google - Existing user');
         // event login - add later
