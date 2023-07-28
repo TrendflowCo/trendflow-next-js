671a6b511dfa005a41affc6c9ef51c496a6bc681
Julian Lopez Baasch <julianlopezba@gmail.com>
2023-07-28 23:54:13 +0200

    Adding GTM snippet

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,28 @@ import '../styles/globals.css';
 import Layout from '../components/global/LayOut';
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
+    <>      
+    <Script id="google-site-tag"
+    strategy='lazyOnload'
+    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_ID}`}
+    />
+    <Script id="google-analytics" strategy='lazyOnload'>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.GOOGLE_TAG_ID}');
+      `}
+    </Script>
     <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </Provider>
+    </>
   ) 
 }
