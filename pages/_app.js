10f6caa60a766fff68a8e236df7c78e92f5ffa0b
artuknees <artuknees@gmail.com>
2023-09-17 18:24:52 -0300

    added first blog format

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import Layout from '../components/global/LayOut';
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
