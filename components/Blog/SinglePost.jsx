3b9b9745ad6b27f75d585e033ee8f63815ced306
artuknees <artuknees@gmail.com>
2023-09-19 09:30:42 -0300

    identify spaces into lines

--- components/Blog/SinglePost.jsx
+++ components/Blog/SinglePost.jsx
@@ -1,8 +1,8 @@
 import { CardMedia } from "@mui/material";
-import React from "react";
+import React, { Fragment } from "react";
 
 const SinglePost = ({post}) => {
-    console.log('post: ', post)
+    console.log('post: ', post.text.split('\\n'))
     function formatDateFromTimestamp(timestampInMilliseconds, locale = 'en-US', options = {
         year: 'numeric',
         month: 'long',
@@ -13,13 +13,21 @@ const SinglePost = ({post}) => {
       }
     return (
         <div className="bg-dokuso-white text-dokuso-black rounded-[12px] overflow-hidden flex flex-row shadow-lg w-[95%] mx-auto mt-4 mb-4">
-            <div className="p-4 w-fit">
+            <div className="p-4 w-[60%]">
                 <div className="text-dokuso-pink font-semibold text-sm mb-2">
                     {formatDateFromTimestamp(post.date.seconds*1000)}
                 </div>
                 <h1 className="text-2xl font-semibold mb-2">{post.title}</h1>
                 <h3 className="text-lg font-normal mb-2">{post.subTitle}</h3>
-                <section className="text-sm text-dokuso-black mb-4 whitespace-pre-wrap">{post.text}</section>
+                {/* <section className="text-sm text-dokuso-black mb-4 whitespace-pre-wrap">{post.text}</section> */}
+                <section className="text-sm text-dokuso-black mb-4">{post.text.split('\\n').map((item,index)=> {
+                    return (
+                        <Fragment key={index}>
+                            {item}<br/>
+                        </Fragment>
+                    )
+                })}</section>
+
 
                 <div className="text-dokuso-black font-medium text-sm">
                 By {post.author}
