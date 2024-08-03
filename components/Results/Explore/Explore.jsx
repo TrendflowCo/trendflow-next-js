702bbdfa130887d22f23939814503e9c2873961c
Julian Lopez Baasch <julianlopezba@gmail.com>
2024-08-04 00:51:05 +0200

    FIx for deploying

--- components/Results/Explore/Explore.jsx
+++ components/Results/Explore/Explore.jsx
@@ -42,6 +42,7 @@ import DialogActions from '@mui/material/DialogActions';
 import ProductImageGallery from './ProductImageGallery';
 import CheckIcon from '@mui/icons-material/Check';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import Link from 'next/link';
 
 const Explore = () => {
     const router = useRouter();
@@ -149,12 +150,12 @@ const Explore = () => {
                                 <nav className="flex mb-4" aria-label="Breadcrumb">
                                   <ol className="inline-flex items-center space-x-1 md:space-x-3">
                                     <li className="inline-flex items-center">
-                                      <a href="/" className="text-gray-700 hover:text-trendflow-pink">Home</a>
+                                      <Link href="/" className="text-gray-700 hover:text-trendflow-pink">Home</Link>
                                     </li>
                                     <li>
                                       <div className="flex items-center">
                                         <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
-                                        <a href="/results" className="ml-1 text-gray-700 hover:text-trendflow-pink md:ml-2">Results</a>
+                                        <Link href="/results" className="ml-1 text-gray-700 hover:text-trendflow-pink md:ml-2">Results</Link>
                                       </div>
                                     </li>
                                     {currentProduct?.brand && (
