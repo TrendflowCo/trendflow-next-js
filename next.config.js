eb92ea022035ef4e02705893d67804e3f2322f1a
Julian López <julian@MacBook-Pro-de-Julian.local>
2024-08-03 23:56:03 +0200

    Updating next.config

--- next.config.js
+++ next.config.js
@@ -6,141 +6,27 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'api.producthunt.com'
+        hostname: '**.com',
       },
       {
         protocol: 'https',
-        hostname: 'upload.wikimedia.org'
+        hostname: '**.org',
       },
       {
         protocol: 'https',
-        hostname: 'ww1.freelogovectors.net'
+        hostname: '**.net',
       },
       {
         protocol: 'https',
-        hostname: 'cdn.shopify.com'
+        hostname: '**.no',
       },
       {
         protocol: 'https',
-        hostname: 'seeklogo.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.pacificplace.com.hk'
-      },
-      {
-        protocol: 'https',
-        hostname: 'torrado.es'
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'logodownload.org'
-      },
-      {
-        protocol: 'https',
-        hostname: 'logos-download.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'fashionunited.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.squarespace-cdn.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.ink-clothing.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'adanola.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'danielleguiziony.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'geel.us'
-      },
-      {
-        protocol: 'https',
-        hostname: 'gimaguas.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'kitteny.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.licdn.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'modemischiefstudios.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'nodress67.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'vanessamooney.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'withjean.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'troisthelabel.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'shop.amlul.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'maisxfrida.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.getinspired.no'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.storets.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.disturbia.co.uk'
-      },
-      {
-        protocol: 'https',
-        hostname: '2.bp.blogspot.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.crunchbase.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.sweetlemonboutique.com'
-      },
-    ]
+        hostname: '**.co.uk',
+      }
+      // Add more patterns as needed
+    ],
   }
-}
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
